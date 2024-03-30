bd8078b62f533d3831e1f36f0eff627f3afe1b49
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-03-30 16:28:37 +0700

    add table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Header from "@/components/Header";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { daylist } from "@/lib/utils";
 function App() {
   return (
     <div className="bg-slate-100 min-h-screen flex">
@@ -8,6 +17,29 @@ function App() {
         <h1 className="text-3xl font-semibold">Call Plan Management</h1>
 
         <Header />
+        <div className="w-9/12 mx-auto mt-10">
+          <Table>
+            <TableCaption>
+              Call plan week 10 for Medical Representative : Wirayuda Senjaya.
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                {daylist.map((day) => (
+                  <TableHead className="text-center">{day}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                {Array.from({ length: 5 }, (_, index) => (
+                  <TableCell key={index} className="text-center">
+                    {index + 20}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
         <div className="w-52"></div>
       </div>
     </div>
